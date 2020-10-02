bad29ea9c883e7856ccf512125ac8ccb9eff858b
Justice Payne <spikepayne@yahoo.com>
2020-10-02 22:54:03 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,8 +2,7 @@ require 'pry'
 
 class Scraper	class Scraper
 
-  def self.scrape_index_page(index_url)	  def self.scrape_index_page(index_url)
-
+  def self.scrape_index_page(index_url)
     students_hash = []
     html = Nokogiri::HTML(open(index_url))
     html.css(".student-card").collect do |student|
@@ -18,8 +17,7 @@ class Scraper	class Scraper
   end
 
 
-  def self.scrape_profile_page(profile_url)	  def self.scrape_profile_page(profile_url)
-
+  def self.scrape_profile_page(profile_url)
     students_hash = {}
   end	
 
